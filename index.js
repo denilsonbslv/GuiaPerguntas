05571b1d33c31bdd1c2d583f7335ffe1ebca610f
Denilson B Silva <89758699+denilsonbslv@users.noreply.github.com>
2021-10-12 19:37:45 -0300

    * Testando laços de repetição e condição *

--- index.js
+++ index.js
@@ -5,7 +5,24 @@ const app = express();
 app.set('view engine','ejs');
 
 app.get("/", (req, res) => {
-    res.render("index");
+    var nome = "denilson";
+    var lang = "Python";
+    var exibirMsg = false;
+
+    var Produtos = [
+        {nome: "Doritos",preco: 3.15},
+        {nome: "Coca-cola",preco: 4},
+        {nome: "Leite",preco: 1.45}
+    ]
+
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        empresa: "Iskista Atakado",
+        inscritos: 15,
+        msg: exibirMsg,
+        produtos: Produtos
+    });
 });
 
 app.listen(8080, ()=>{
