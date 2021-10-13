a8b559c7de82ecd901b437011c4fa286e8004418
denilsonbslv <denilson.barboza@iskisita.com.br>
2021-10-13 10:18:50 -0300

    *Criação da pág. de perguntas e dos partials *

--- index.js
+++ index.js
@@ -3,27 +3,16 @@ const app = express();
 
 // Estou dizendo para o Express usar o EJS como view engine
 app.set('view engine','ejs');
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-    var nome = "denilson";
-    var lang = "Python";
-    var exibirMsg = false;
-
-    var Produtos = [
-        {nome: "Doritos",preco: 3.15},
-        {nome: "Coca-cola",preco: 4},
-        {nome: "Leite",preco: 1.45}
-    ]
-
     res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Iskista Atakado",
-        inscritos: 15,
-        msg: exibirMsg,
-        produtos: Produtos
     });
 });
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
+})
 
 app.listen(8080, ()=>{
     console.log("App rodando!")
